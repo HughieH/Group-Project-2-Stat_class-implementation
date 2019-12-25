2fb70dda0ebee08526ff9dabf9156f33855381cf
cturtl-dan <turtleturtlerunrun@gmail.com>
2019-12-24 18:17:25 -0800

    added max, min, mean, stdD, median, and mirrorPrint throught terminal. -dan

--- LinearStructures/dllist.h
+++ LinearStructures/dllist.h
@@ -8,7 +8,10 @@
 
 #include "list.h"
 #include "dlink.h"
+#include "utilities.h"
+#include <cmath>
 #include <iostream>
+#include <vector>
 
 //test - daniel
 
@@ -19,6 +22,12 @@ private:
   DLink<E>* tail; // Pointer to last element
   DLink<E>* curr; // Access to current element
   int cnt; // Size of list
+
+  std::vector<E> count_array;
+  E mode, min, max;
+  double median, mean, std_deviation;
+
+
   void init() { // Initialization helper method
     curr = head = new DLink<E>;
     head->next = tail = new DLink<E>(head, NULL);
@@ -88,6 +97,7 @@ public:
   void next(){
     if (curr != tail) curr = curr->next;
   }
+  
   int length() const {
     return cnt; } // Return length
   // Return the position of the current element
@@ -109,10 +119,126 @@ public:
     return curr->next->element;
   }
 
-  void reverse(){
+  void reverse() {}
 
-  }
   bool isAtEnd() { return curr == tail; }
+  bool isAtBegin() { return curr == head; }
+
+
+  // a testing method purposed for getting used to printing 
+  // a doubly linked list, time complexity: O(n)
+  void printMirrorDLLIST() {
+    std::cout << "printing mirror image." << std::endl;
+    std::cout << std::endl;
+
+    curr = head->next;
+    while(!isAtEnd()) {
+      std::cout << curr->element << std::endl;
+      curr=curr->next;
+    }
+
+    std::cout << "\nnow printing in reverse." << std::endl;
+
+    curr = tail->prev;
+    while(!isAtBegin()) {
+      std::cout << curr->element << std::endl;
+      curr=curr->prev;
+    }
+  }
+
+  // element(s) of the data set which has(have) the
+  // most occurance in the data set
+  E get_mode() {
+    curr = head->next;
+    while (!isAtEnd()) {
+      count_array.pushback(curr->element);
+      curr=curr->next;
+    }
+  }
+
+  // middle of the data set, if the data size is odd, 
+  // its physically in the middle. else if data size is even,
+  // the median will be between the middle two element 
+  double get_median() { //! must have a sorted dllist first
+                        //! can be both decending or ascending
+    curr = head->next;
+    if (cnt & 1) // bit-wise isOdd for faster performance
+    {
+      for (size_t i = 0; i < (cnt/2); i++)
+      {
+        curr=curr->next;
+      }
+      median=curr->element;
+      return curr->element;
+    }
+    else
+    {
+      for (size_t i = 0; i < (cnt/2) - 1; i++)
+      {
+        curr=curr->next; 
+      }
+      std::cout << "\ncurr and currnext number is: " <<curr->element << " " << curr->next->element << "\n";
+      double result=(double)(curr->element + curr->next->element) / 2;
+      median=result;
+      return result;
+    }
+  }
+  
+  // the mean is the average and is computed
+  // as the sum of all the element in the data set
+  // divied by the data size 
+  E get_mean() {
+    E sum = 0;
+    curr = head->next;
+    while (!isAtEnd()) 
+    {
+      sum += curr->element;
+      curr=curr->next;
+    }
+    mean=sum/cnt;
+    return mean;
+  }
+
+  //! get_mean must be called prior to this call because
+  //! we need the most updated mean
+  // the measuremeant of the amount of variation or
+  // dispersion of the data set
+  double get_std_deviation() {
+    double std_d=0;
+    curr = head->next;
+    while (!isAtEnd()) {
+      std_d = pow(curr->element - mean, 2);
+      curr=curr->next;
+    }
+    std_deviation = sqrt(std_d / cnt);
+    return std_deviation;
+  }
+
+  E get_max() {
+    curr = head->next;
+    E max = curr->element;
+    while (!isAtEnd()) {
+      if(max < curr->element)
+      {
+        max=curr->element;
+      }
+      curr=curr->next;
+    }
+    return max;
+  }
+
+  E get_min() {
+    curr = head->next;
+    E min = curr->element;
+    while (!isAtEnd()) {
+      if (min > curr->element)
+      {
+        min=curr->element;
+      }
+      curr=curr->next;
+    }
+    return min;
+  }
 
 };
 
--- LinearStructures/main.cpp
+++ LinearStructures/main.cpp
@@ -1,86 +1,57 @@
 #include "link.h"
 #include "dlink.h"
-#include "alist.h"
+// #include "alist.h"
 #include "llist.h"
 #include "dllist.h"
-#include "astack.h"
-#include "lstack.h"
-#include "aqueue.h"
-#include "lqueue.h"
+// #include "astack.h"
+// #include "lstack.h"
+// #include "aqueue.h"
+// #include "lqueue.h"
 
 #include <iostream>
 
-//template <typename E>
-//DLink<E>* DLink<E>::freelist = NULL;  // The freelist head pointer is actually created here
+// template <typename E>
+// DLink<E>* DLink<E>::freelist = NULL;  // The freelist head pointer is actually created here
 
-/*
+
 int main(int argc, char *argv[])
 {
-  std::cout << "All is good" << "\n";
+  // std::cout << "All is good" << "\n";
 
-  AList<int> int_array_list(20);
-  LList<int> int_linked_list;
+  // AList<int> int_array_list(20);
+  // LList<int> int_linked_list;
   DLList<int> int_doubly_linked_list;
 
-  for (int i = 0; i < 20; i++)
-    {
-      int_array_list.append(i);// Try this with i > 20 and se that at i = 21 (> maxSize) you will see the assertion failing with the message "Assertion Failed: List capacity exceeded"
-      int_linked_list.append(i);// Default size is 10 but no warning as list allow dynamic resizing
-      int_doubly_linked_list.append(i);// Default size is 10 but no warning as list allow dynamic resizing
-    }
+  for (int i = 1; i < 12; i++)
+  {
+    int_doubly_linked_list.append(i);// Default size is 10 but no warning as list allow dynamic resizing
+  }
 
-  std::cout << "In the array list, the current length is: " <<int_array_list.length() << "  current node, " <<   int_array_list.remove() << " has been removed" << " the new length is: " <<int_array_list.length() <<"\n";
+  // std::cout << "In the array list, the current length is: " <<int_array_list.length() << "  current node, " <<   int_array_list.remove() << " has been removed" << " the new length is: " <<int_array_list.length() <<"\n";
 
-  std::cout << "In the singly linked list, the current length is: " <<int_linked_list.length() << "  current node, " <<   int_linked_list.remove() << " has been removed" << " the new length is: " <<int_linked_list.length() <<"\n";
+  // std::cout << "In the singly linked list, the current length is: " <<int_linked_list.length() << "  current node, " <<   int_linked_list.remove() << " has been removed" << " the new length is: " <<int_linked_list.length() <<"\n";
 
-  std::cout << "In the doubly linked list, the current length is: " <<int_doubly_linked_list.length() << "  current node, " <<   int_doubly_linked_list.remove() << " has been removed" << " the new length is: " <<int_doubly_linked_list.length() <<"\n";
+  std::cout << "In the doubly linked list, the current length is: " 
+    <<int_doubly_linked_list.length() << "\n";
 
-  int_array_list.append(100);
-  int_linked_list.append(100);
-  int_doubly_linked_list.append(100);
+  /* test printing mirror */
+  // int_doubly_linked_list.printMirrorDLLIST();
 
-  std::cout << "In the array list, the current length is: " <<int_array_list.length() << "  current node, " <<   int_array_list.remove() << " has been removed" << " the new length is: " <<int_array_list.length() <<"\n";
+  // std::cout << "In the array list, the current length is: " <<int_array_list.length() << "  current node, " <<   int_array_list.remove() << " has been removed" << " the new length is: " <<int_array_list.length() <<"\n";
 
-  std::cout << "In the singly linked list, the current length is: " <<int_linked_list.length() << "  current node, " <<   int_linked_list.remove() << " has been removed" << " the new length is: " <<int_linked_list.length() <<"\n";
+  // std::cout << "In the singly linked list, the current length is: " <<int_linked_list.length() << "  current node, " <<   int_linked_list.remove() << " has been removed" << " the new length is: " <<int_linked_list.length() <<"\n";
 
-  std::cout << "In the doubly linked list, the current length is: " <<int_doubly_linked_list.length() << "  current node, " <<   int_doubly_linked_list.remove() << " has been removed" << " the new length is: " <<int_doubly_linked_list.length() <<"\n";
-
-  std::cout << "\n\nStarting stack demo with string reverse ...." << "\n \n";
-
-  // Stack demo. String reverse using stack. You can also use this to check for a Palindrome
-  LStack <char> str1;
-  std::string input_string = "programming";
-  for (int i = 0; i < input_string.length(); i++){
-      str1.push(input_string.at(i));
-    }
-  std::string reversed_string = "";
-
-  for (int i = 0; i < input_string.length(); i++){
-      reversed_string += str1.pop(); // Building the reversed string as we pop
-    }
-  std::cout << "Reversed string for the string " <<input_string << " is " << reversed_string << std::endl; // print reversed
-  std::cout << std::endl;
-
-    std::cout << "\n\nStarting queue demo ...." << "\n \n";
-
-  // Queue demo
-  LQueue <int> int_stack;
-  for (int i = 0; i < 10; i++){
-      int_stack.enqueue(i*i); // Insert
-    }
-
-  for (int i = 0; i < 10; i++){
-      std::cout << "Removed " << i << " th " << " value is " << int_stack.dequeue() << std::endl; // Dequeue operation
-    }
+  /* testing min max*/
+  // int min = int_doubly_linked_list.get_min();
+  // int max = int_doubly_linked_list.get_max();
+  // std::cout << "\nThe min of my dllist is: " <<  min << std::endl;
+  // std::cout << "\nThe max of my dllist is: " <<  max << std::endl;
 
 
-  std::cout << "All is good" << "\n";
+
+  // int the_mean = int_doubly_linked_list.get_mean();
+  // std::cout << "the mean of this doubly llist is: " << the_mean << std::endl;
+return 0;
 
 }
-*/
 
-int main() {
-    
-    
-    
-}
